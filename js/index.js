093443f55db53b1f9c635204d043b83337d44132
Sahil Rajesh Shriwardhankar <102847495+notsointresting@users.noreply.github.com>
2024-12-22 13:41:37 +0000

    Completed first iteration

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 // Iteration 1: Names and Input
+let hacker1 = 'Joe'
+console.log("The driver's name is "+hacker1);
+
+let hacker2 = 'John'
+console.log("The navigator's name is "+hacker2);
 //
 // Iteration 2: Conditionals
 
